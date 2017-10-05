c426be1b1dbe90be6b3464293ec7de7813e8bcea
jonnyf89 <jonathan.gorselodge@gmail.com>
2017-10-05 09:15:24 +0100

    Uploaded the entire website to the repositry

---
+++ myscripts.js
@@ -0,0 +1,35 @@
+function clicked(){
+alert('Thank you! I have received your email address and will send you an up to date CV as soon as possible.');
+}
+/*	
+function checkEmail(){
+	var str = document.forms["contact.hmtl"]["Email"].value;
+	var n = str.indexOf('@');
+	var strLength = str.length;
+	var dot = str.indexOf(".",n+1);
+	
+	if (dot == -1){
+		alert ("Email address is missing the dot, please try again.");
+	    return false;
+	}
+	else{
+		return true;
+	}
+}
+
+function clicked(){
+	var str = document.forms["contact.hmtl"]["Email"].value;
+	var n = str.indexOf('@');
+	var strLength = str.length;
+	var dot = str.indexOf(".",n+1);
+	
+	if (dot == -1){
+		alert ("Email address is missing the dot, please try again.");
+	    return false;
+	}
+	else{
+		alert('Thank you! I have received your email address and will send you an up to date CV as soon as possible.');
+		return true;
+	}
+}
+*/
